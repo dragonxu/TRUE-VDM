6d08ce7d7fbf69ae973eb95bbcac31282f0902cf
rocketer <tsathit40@hotmail.com>
2018-10-03 16:33:49 +0700

    Add UC_CommonUI

---
+++ VDM/VDM/Front_UI/UC_CommonUI.ascx
@@ -0,0 +1,35 @@
+﻿<%@ Control Language="vb" AutoEventWireup="false" CodeBehind="UC_CommonUI.ascx.vb" Inherits="VDM.UC_CommonUI" %>
+
+<input type="button" style="position:absolute; bottom:0; left:0; width:100px; height:100px; background-color:red; z-index:10;" onclick="inputPasscodeLeft();" />
+<input type="button" style="position:absolute; bottom:0; right:0; width:100px; height:100px; background-color:red; z-index:10;" onclick="inputPasscodeRight();" />
+<script type="text/javascript">
+
+    var userPasscode = "";
+    var passcodeTime = 0;
+    var truePasscode = "11221";
+
+    function resetPasscode() {
+      
+    }
+
+    function inputPasscodeLeft() {
+        userPasscode += '1';
+    }
+
+    function inputPasscodeRight() {
+        userPasscode += '2';
+    }
+
+    function checkPasscode() {
+        if (userPasscode == truePasscode) {
+            location.href = '../Machine_Console/Login.aspx?KO_ID=<% Response.Write(KO_ID) %>';
+        }
+    }
+
+    var passcodeTimer = function () {
+        userPasscode = '';
+    }
+    setInterval(passcodeTime, 5000);
+
+
+</script>
---
+++ VDM/VDM/Front_UI/UC_CommonUI.ascx.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Data.SqlClient
+
+Public Class UC_CommonUI
+    Inherits System.Web.UI.UserControl
+
+    Dim BL As New VDM_BL
+
+    Public ReadOnly Property KO_ID
+        Get
+            Try
+                Return Request.Cookies("KO_ID").Value
+            Catch ex As Exception
+                Return 0
+            End Try
+        End Get
+    End Property
+
+    Public ReadOnly Property TXN_ID As Integer
+        Get
+            Try
+                Return Session("TXN_ID")
+            Catch ex As Exception
+                Return 0
+            End Try
+        End Get
+    End Property
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Exit Sub
+
+        If KO_ID = 0 Then
+            Response.Redirect("../Machine_Console/Login.aspx")
+            Response.End()
+        End If
+
+        If TXN_ID > 0 Then
+            BL.Update_Service_Transaction(TXN_ID, Me.Page) '-------------- Update ทุกหน้า ------------
+        End If
+        '---------------- Check Shift Open ----------------
+        Dim DT As New DataTable
+        Dim SQL As String = "EXEC dbo.SP_CURRENT_OPEN_SHIFT " & KO_ID
+        Dim DA As New SqlDataAdapter(SQL, BL.ConnectionString)
+        DA.Fill(DT)
+        If DT.Rows.Count = 0 Then
+            '------------- Shift ยังไม่เปิด -------------
+            Response.Redirect("../Machine_Console/Login.aspx?KO_ID=" & KO_ID)
+            Response.End()
+        End If
+
+    End Sub
+
+End Class
