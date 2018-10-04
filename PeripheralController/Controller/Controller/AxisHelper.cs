e2134ba1ea2ef4896674da91d05b48070c7d6013
Maytee Palamee <m.palami@hotmail.com>
2018-10-04 20:32:51 +0700

    add plc controller
    Signed-off-by: Maytee Palamee <m.palami@hotmail.com>

---
+++ PeripheralController/Controller/Controller/AxisHelper.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    /// <summary>
+    /// AxisHelper class
+    /// </summary>
+    public class AxisHelper
+    {
+        /// <summary>
+        /// AxisAccording
+        /// </summary>
+        /// <param name="Omron">Omron lib</param>
+        /// <param name="axis">axis type [x,y]</param>
+        /// <returns></returns>
+        public AxisState AxisAccording(OmronTCP Omron)
+        {
+            AxisState state = AxisState.Unknown;
+            try { 
+
+            }catch(Exception e)
+            {
+                Console.WriteLine("AxisAccording execption : " + e);
+            }
+            return state;
+        }
+        private AxisState AnalyzeAxis(OmronTCP.Hex[] reply, int[] compareBit)
+        {
+            AxisState results = AxisState.Success;
+            try
+            {
+                /*overload x bit 1(0x02), y bit 2(0x04)*/
+                /*timeout x bit9(0x200), y bit10(0x400)*/
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("execption : " + e);
+            }
+            return results;
+        }
+        /// <summary>
+        /// initial device
+        /// </summary>
+        /// <param name="Omron">Omron component</param>
+        /// <returns>Axis State</returns>
+        public async Task<AxisState> HomeAccroding(OmronTCP Omron)
+        {
+            AxisState state = AxisState.Success;
+            try
+            {
+                int[] _positionIni;
+                do
+                {
+                    Omron_Response_Code Response = Omron_Response_Code.Unknown;
+                    Array _homeRead = Omron.Read(Omron_Command_Header_Read.CIO_Area_Read, 4213, 1, ref Response, Omron_Data_Type.Integer);
+                    _positionIni = (int[])_homeRead;
+                    Thread.Sleep(200);
+                    Console.WriteLine("position : " + _positionIni[0]);
+
+                    Array reply = Omron.Read(Omron_Command_Header_Read.CIO_Area_Read, 4206, 3, ref Response, Omron_Data_Type.Hex);
+                    OmronTCP.Hex[] results = (OmronTCP.Hex[])reply;
+                    state = Identify(results);
+                    Console.WriteLine("current state : " + state);
+                    if (!state.Equals(AxisState.Success))
+                    {
+                        state = AxisState.Error;
+                        Console.WriteLine("Execption : " + state);
+                        break;
+                    }
+                } while (_positionIni[0] != 0);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Execption : " + e);
+            }
+            return state;
+        }
+        /// <summary>
+        /// Position According
+        /// </summary>
+        /// <param name="Omron">Omron Lib</param>
+        /// <returns>AxisState</returns>
+        public AxisState PostionAccording(OmronTCP Omron)
+        {
+            AxisState state = AxisState.Success;
+            try
+            {
+                int[] _Axis;
+                do
+                {
+                    //4201 error, 4206 error timeout
+                    /*206 return 1 == error ,0 == ok, timeout ===*/
+                    /*213 return 1 == inprocess, 0 == home position*/
+                    Omron_Response_Code Response = Omron_Response_Code.Unknown;
+                    Array _read = Omron.Read(Omron_Command_Header_Read.CIO_Area_Read, 4201, 2, ref Response, Omron_Data_Type.Integer);
+                    _Axis = (int[])_read;
+                    Thread.Sleep(200);
+
+                    Console.WriteLine("position[word 1] : " + _Axis[0]);
+                    Console.WriteLine("position[word 2] : " + _Axis[1]);
+
+                    Array reply = Omron.Read(Omron_Command_Header_Read.CIO_Area_Read, 4206, 3, ref Response, Omron_Data_Type.Hex);
+                    OmronTCP.Hex[] hexaDecimal = (OmronTCP.Hex[])reply;
+
+                    state = Identify(hexaDecimal);
+                    if (!state.Equals(AxisState.Success))
+                    {
+                        Console.WriteLine("Execption : " + state);
+                        break;
+                    }
+                } while (_Axis[0] != 0 && _Axis[1] != 0); // 2 word
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Execption : " + e);
+            }
+            return state;
+        }
+
+        private AxisState Identify(OmronTCP.Hex[] reply)
+        {
+            AxisState state = AxisState.Success;
+            for(int index=0; index < reply.Length; index++)
+            {
+                if (reply[index].ToInt != 0)
+                {
+                    /*alram x bit 1(0x02), y bit 2(0x04)*/
+                    /*timeout x bit9(0x200), y bit10(0x400)*/
+                    Console.WriteLine("error 0 ");
+                    state = AxisState.Error;
+                    if ((reply[index].ToInt & 0x200) != 0)
+                    {
+                        Console.WriteLine("Timeout x");
+                        state = AxisState.TimeoutAxisx;
+                    }
+                    if ((reply[index].ToInt & 0x400) != 0)
+                    {
+                        Console.WriteLine("Timeout y");
+                        state = AxisState.TimeoutAxisy;
+                    }
+                    if ((reply[index].ToInt & 0x02) != 0)
+                    {
+                        Console.WriteLine("alram servo x");
+                        state = AxisState.ErrorAxisx;
+                    }
+                    if ((reply[index].ToInt & 0x04) != 0)
+                    {
+                        Console.WriteLine("alram servo y");
+                        state = AxisState.ErrorAxisy;
+                    }
+                }
+            }
+            return state;
+        }
+    }
+}
---
+++ PeripheralController/Controller/Controller/OmronLib/TCPTransport.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Controller
+{
+    public class TCPTransport  
+    {
+        private IPEndPoint _endPoint = null;
+        private Socket _socket = null;
+        private Int32 _timeout = 2000;
+        private Ping _ping = null;
+        private PingReply _pingReply = null;
+
+        public IPAddress RemoteIP
+        {
+            get { return this._endPoint.Address; }
+            set { this._endPoint.Address = value; }
+        }
+        public int RemotePort
+        {
+            get { return this._endPoint.Port; }
+            set { this._endPoint.Port = value; }
+        }
+
+        public int ReadTimeout
+        {
+            get { return this._timeout; }
+            set { this._timeout = value; }
+        }
+        public bool IsOpen
+        {
+            get
+            { return (this._socket != null); }
+        }
+        public bool Connected
+        {
+            get
+            {
+                if (this._socket == null)
+                    return false;
+                else
+                    return this._socket.Connected;
+            }
+        }
+
+        public TCPTransport()
+        {
+            int back = 0;
+            this._ping = new Ping();
+            this._endPoint = new IPEndPoint(0, 0);
+        }
+        
+        public bool Open()
+        {
+            this._socket = new Socket(_endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            this._socket.SendTimeout = this._timeout;
+            this._socket.ReceiveTimeout = this._timeout;
+            this._socket.Connect(this._endPoint);
+            return this.Connected;
+        }
+        public void Close()
+        {
+            if (this._socket == null) return;
+            if (this.Connected)
+            {
+                this._socket.Disconnect(false);
+                this._socket.Close();
+            }
+            this._socket = null;
+        }
+        public void Write(string command)
+        {
+            this.Write(System.Text.Encoding.ASCII.GetBytes(command), command.Length);
+        }
+        public void Write(byte[] command)
+        {
+            this.Write(command, command.Length);
+        }
+        public void Write(byte[] command, int cmdLen)
+        {
+            if (!this.Connected)
+            {
+                throw new Exception("Socket is not connected.");
+            }
+            int bytesSent = this._socket.Send(command, cmdLen, SocketFlags.None);
+            if (bytesSent != cmdLen)
+            {
+                string msg = string.Format("Sending error. (Expected bytes: {0}  Sent: {1})", cmdLen, bytesSent);
+                throw new Exception(msg);
+            }
+        }
+        public int Read(char[] response, int respLen)
+        {
+            return this.Read(response, 0, respLen);
+        }
+        public int Read(char[] response, int offset, int respLen)
+        {
+            byte[] bytes = System.Text.Encoding.Unicode.GetBytes(response);
+            int retLen = this.Read(bytes, offset, respLen);
+            response = System.Text.Encoding.Unicode.GetChars(bytes);
+            return retLen;
+        }
+        public int Read(byte[] response, int respLen)
+        {
+            return this.Read(response, 0, respLen);
+        }
+        public int Read(byte[] response, int offset, int respLen)
+        {
+            if (!this.Connected)
+            {
+                throw new Exception("Socket is not connected.");
+            }
+            int bytesRecv = this._socket.Receive(response, offset, respLen, SocketFlags.None);
+            if (bytesRecv != respLen)
+            {
+                string msg = string.Format("Receiving error. (Expected: {0}  Received: {1})", respLen, bytesRecv);
+            }
+            return bytesRecv;
+        }
+        //public bool Ping()
+        //{
+        //    if (this._endPoint.Address == null) return false;
+        //    this._pingReply = this._ping.Send(this._endPoint.Address, this._timeout);
+        //    return (this._pingReply.Status == IPStatus.Success) ? true : false;
+        //}
+    }
+}
