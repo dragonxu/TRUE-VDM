d403ac2f952576ff5e6caa1c9d0b58891a0ebb2c
rocketer <tsathit40@hotmail.com>
2018-10-09 12:30:20 +0700

    เตรียมทำ True Money

---
+++ PeripheralController/PeripheralController/DisableCash.aspx.vb
@@ -0,0 +1,51 @@
+﻿Public Class DisableCash
+    Inherits System.Web.UI.Page
+
+    Dim BL As New Core_BL
+
+    Dim Cash As CashReceiver.CashReceiver = Nothing
+    Dim Coin As CoinReciever.CoinReciever = Nothing
+
+    Private ReadOnly Property callBackFunction As String
+        Get
+            Try
+                Return Request.QueryString("callback")
+            Catch ex As Exception
+                Return ""
+            End Try
+        End Get
+    End Property
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Cash = New CashReceiver.CashReceiver
+        Coin = New CoinReciever.CoinReciever
+
+        Cash.SetPort(BL.CashReciever_Port)
+        Coin.SetPort(BL.CoinReciever_Port)
+
+        Try
+            Cash.Close()
+        Catch : End Try
+
+        Try
+            Coin.Close()
+        Catch : End Try
+
+    End Sub
+
+    Private Sub callBack()
+        Try
+            Cash.Close()
+        Catch : End Try
+        Try
+            Coin.Close()
+        Catch : End Try
+
+        Dim Script As String = callBackFunction & "('success');"
+        Response.Write(Script)
+        Response.End()
+
+    End Sub
+
+End Class
---
+++ PeripheralController/PeripheralController/Print.aspx.vb
@@ -0,0 +1,99 @@
+﻿Public Class Print
+    Inherits System.Web.UI.Page
+
+    Private ReadOnly Property Mode
+        Get
+            Try
+                Return Request.QueryString("Mode")
+            Catch ex As Exception
+                Return ""
+            End Try
+        End Get
+    End Property
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Select Case Mode
+            Case "Print"
+                Print()
+            Case "GetList"
+                '------------- Send Contents To Printer------------
+                Dim ThePrinter As New Printer.Printer
+                Dim _list As String() = ThePrinter.GetList
+                For i As Integer = 0 To _list.Count - 1
+                    Response.Write(_list(i) & "<br/>")
+                Next
+                Response.End()
+        End Select
+
+    End Sub
+
+    Private Sub Print()
+        If Request.ContentType <> "text/xml" Then Exit Sub
+        'Throw New HttpException(500, "Unexpected Content-Type")
+
+        Dim Reader = New IO.StreamReader(Request.InputStream)
+        Request.ContentEncoding = Encoding.UTF8
+        Request.InputStream.Seek(0, IO.SeekOrigin.Begin)
+        Reader.DiscardBufferedData()
+        Dim XML As String = XDocument.Load(Reader).ToString
+
+        Dim C As New Converter
+        Dim BL As New Core_BL
+        Dim Contents As DataTable = C.XMLToDatatable(XML)
+
+        '------------- Generate Result ------------
+        Dim Result = New DataTable
+        Result.Columns.Add("status", GetType(Boolean))
+        Result.Columns.Add("message", GetType(String))
+        Dim DR As DataRow = Result.NewRow
+        Result.Rows.Add(DR)
+
+        '------------- Send Contents To Printer------------
+        Dim ThePrinter As New Printer.Printer
+
+        '------------- Check Current Status ---------------
+        Select Case ThePrinter.GetStatus(BL.Printer_Name)
+            Case Printer.Printer.PrinterStatus.Offline
+                DR("status") = False
+                DR("message") = "Printer Offline"
+                Response.Write(SingleRowDataTableToJSON(Result))
+                Response.End()
+                Exit Sub
+            Case Printer.Printer.PrinterStatus.Unknow
+                DR("status") = False
+                DR("message") = "Printer Unknow Status"
+                Response.Write(SingleRowDataTableToJSON(Result))
+                Response.End()
+                Exit Sub
+        End Select
+
+
+        Dim Lines As New List(Of Printer.Printer.ContentLine)
+        Try
+            '------- Transalate DataTable To Printer Contents--------
+            For i As Integer = 0 To Contents.Rows.Count - 1
+                Dim Line As New Printer.Printer.ContentLine
+                With Line
+                    .Text = Contents.Rows(i).Item("Text").ToString
+                    .ImagePath = Contents.Rows(i).Item("ImagePath").ToString
+                    .FontSize = Contents.Rows(i).Item("FontSize")
+                    .FontName = Contents.Rows(i).Item("FontName")
+                    .Bold = Contents.Rows(i).Item("Bold")
+                    .IsColor = Contents.Rows(i).Item("IsColor")
+                    .ContentType = CType(Contents.Rows(i).Item("ContentType"), Printer.Printer.ContentType)
+                End With
+                Lines.Add(Line)
+            Next
+            ThePrinter.Print(BL.Printer_Name, Lines)
+            DR("status") = True
+            DR("message") = "success"
+        Catch ex As Exception
+            DR("status") = False
+            DR("message") = ex.Message
+        End Try
+        Response.Write(SingleRowDataTableToJSON(Result))
+        Response.End()
+    End Sub
+
+End Class
